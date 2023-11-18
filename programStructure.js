38a3467fb130bc9e3a6b1fd2b8e05f33b9b2e99a
Vsemv <vsemeniuk999@gmail.com>
2023-11-18 22:15:27 +0200

    continue from cycle for

---
+++ programStructure.js
@@ -0,0 +1,44 @@
+            // if
+
+// let num = Number(prompt("Enter the number"));
+// if (num < 10){
+//     console.log("small");
+// }else if (num < 100){
+//     console.log("middle");
+// }else{
+//     console.log("big");
+// }
+
+            // while
+
+// let number = 0;
+// while(number <= 12){
+//     console.log(number);
+//     number = number + 2;
+// };
+
+// let result = 1;
+// let counter = 0;
+// while ( counter < 10 ){
+//     result = result * 2;
+//     counter = counter + 1;
+//     console.log("result: " + result, "counter: " + counter);
+// }
+// console.log(result);
+
+            // do
+
+// let yourName;
+// do {
+//     yourName = prompt("Who are you?");
+// } while (!yourName);
+// console.log(yourName);
+
+let count = 0;
+let expect = 10;
+do {
+    count = count + 1;
+    console.log("count: " + count);
+} while (count < expect);
+console.log("count: " + count, "expect: " + expect);
+
