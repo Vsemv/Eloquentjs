dadf365d4c691b4e939ae959759ae85cced1a5e9
Vsemv <vsemeniuk999@gmail.com>
2023-11-19 22:01:43 +0200

    continue from ex 3 chess board

--- programStructure.js
+++ programStructure.js
@@ -34,11 +34,72 @@
 // } while (!yourName);
 // console.log(yourName);
 
-let count = 0;
-let expect = 10;
-do {
-    count = count + 1;
-    console.log("count: " + count);
-} while (count < expect);
-console.log("count: " + count, "expect: " + expect);
+// let count = 0;
+// let expect = 10;
+// do {
+//     count = count + 1;
+//     console.log("count: " + count);
+// } while (count < expect);
+// console.log("count: " + count, "expect: " + expect);
 
+            // for
+
+// let result = 1;
+// for(let counter = 0; counter < 10; counter = counter +1){
+//     result = result * 2;
+//     console.log("result: " + result);
+// }
+// console.log(result);
+
+// for(let current = 1; ; current = current + 1){
+//     if(current % 7 == 0){
+//         console.log(current + "% from 7 = 0");
+//         break;
+//     }
+//     console.log(current + "% from 7 != 0");
+// }
+
+//          exercises
+
+            // exercise 1
+    // constructing triangle in a cycle:
+// let triangle = '#'
+// for(let i = 0; ; i++){
+//     console.log(triangle);
+//     if(triangle.length == 7){
+//         break;
+//     };
+//     triangle += '#';
+// };
+
+            // exercise 2
+    // FizzBuzz
+// let count = 0;
+// for(let i = 0; i < 100; i++){
+//     count += 1;
+//     if(count % 3 == 0 && count % 5 != 0){
+//         console.log('Fizz');
+//     }else if(count % 5 == 0 && count % 3 == 0){
+//         console.log('FizzBuzz');
+//     } else if(count % 5 == 0 && count % 3 != 0){
+//         console.log('Buzz');
+//     }else{
+//         console.log(count);
+//     };
+// };
+
+            // exercise 3
+    // chess board
+let string = '';
+for(let col = 0; col < 8; col++){
+    for(let row = 0; row < 8; row++){
+        if(col % 2 != 0 || row % 2 == 0){
+            string += ' ';
+        }else if(col % 2 == 0 || row % 2 != 0){
+            string += '#';
+        }
+    }
+    string += '\n';
+}
+console.log(string);
+console.log(string.length);
