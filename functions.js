d12014f97ad3dc39c6327dc16aeb6f2dee9d27e3
Vsemv <vsemeniuk999@gmail.com>
2023-11-28 22:27:00 +0200

    continue from recursion

--- functions.js
+++ functions.js
@@ -136,5 +136,22 @@
 
                 // closing
 
+function wrapValue(n) {
+    let local = n;
+    return () => local;
+};
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+console.log(wrap1());
+console.log(wrap2());
+
+function multiplier(factor) {
+    return number => number * factor;
+};
+let twice = multiplier(2);
+console.log(twice(5));
+
+                // recursion
+
 
                 
