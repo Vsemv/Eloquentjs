6bb966c8ea30c5951bd5dbe308b39d2d66aa2339
Vsemv <vsemeniuk999@gmail.com>
2023-11-29 23:56:39 +0200

    continue do exercise about recursion

--- functions.js
+++ functions.js
@@ -153,57 +153,81 @@
 
                 // recursion
 
-function power(base, exponent) {
-    if (exponent == 0) {
-        return 1;
-    } else {
-        return base * power(base, exponent - 1);
-    }
-}
-console.log(power(2, 3));
+// function power(base, exponent) {
+//     if (exponent == 0) {
+//         return 1;
+//     } else {
+//         return base * power(base, exponent - 1);
+//     }
+// }
+// console.log(power(2, 3));
 
-function findSolution(target) {
-    function find(current, history) {
-        if (current == target) {
-            return history;
-        } else if (current > target) {
-            return null;
-        } else {
-            return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`);
-        }
-    }
-    return find(1, '1');
-}
-console.log(findSolution(33));
+// function findSolution(target) {
+//     function find(current, history) {
+//         if (current == target) {
+//             return history;
+//         } else if (current > target) {
+//             return null;
+//         } else {
+//             return find(current + 5, `(${history} + 5)`) || find(current * 3, `(${history} * 3)`);
+//         }
+//     }
+//     return find(1, '1');
+// }
+// console.log(findSolution(33));
 
                 // expansion of functions
 
-function printFarmInventory(cows, chickens) {
-    let cowString = String(cows);
-    while (cowString.length < 3) {
-        cowString = '0' + cowString;
-    }
-    console.log(`${cowString} коров`);
-    let chickenString = String(chickens);
-    while (chickenString.length < 3) {
-        chickenString = '0' + chickenString;
-    }
-    console.log(`${chickenString} цыплят`);
-}
-printFarmInventory(7, 11);
+// function printFarmInventory(cows, chickens) {
+//     let cowString = String(cows);
+//     while (cowString.length < 3) {
+//         cowString = '0' + cowString;
+//     }
+//     console.log(`${cowString} коров`);
+//     let chickenString = String(chickens);
+//     while (chickenString.length < 3) {
+//         chickenString = '0' + chickenString;
+//     }
+//     console.log(`${chickenString} цыплят`);
+// }
+// printFarmInventory(7, 11);
 
-console.log('divide functions. from down you can see examplae of divided functions');
+// console.log('divide functions. from down you can see examplae of divided functions');
 
-function zeroPad(number, width) {
-    let string = String(number);
-    while (string.length < width) {
-        string = '0' + string;
-    }
-    return string
-}
-function printFarmInventoryExp(cows, chickens, pigs) {
-    console.log(`${zeroPad(cows, 3)} коров`);
-    console.log(`${zeroPad(chickens, 3)} цыплят`);
-    console.log(`${zeroPad(pigs, 3)} свиней`);
+// function zeroPad(number, width) {
+//     let string = String(number);
+//     while (string.length < width) {
+//         string = '0' + string;
+//     }
+//     return string
+// }
+// function printFarmInventoryExp(cows, chickens, pigs) {
+//     console.log(`${zeroPad(cows, 3)} коров`);
+//     console.log(`${zeroPad(chickens, 3)} цыплят`);
+//     console.log(`${zeroPad(pigs, 3)} свиней`);
+// };
+// printFarmInventoryExp(7, 16, 3);
+
+                // functions and side effects
+
+                // exercises
+
+        // exercise 1
+    // minimum
+
+let min = (num1, num2, num3) => {
+    if(num1 < num2 && num1 < num3) {
+        return num1;
+    } else if (num2 < num1 && num2 < num3) {
+        return num2;
+    } else if (num3 < num1 && num3 < num2) {
+        return num3;
+    };
 };
-printFarmInventoryExp(7, 16, 3);
+let minNum = min(2, 1, 3);
+console.log('this number has minimum value: ' + minNum);
+
+        // exercise 2
+    // recursion
+
+
