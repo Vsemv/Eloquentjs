c79a153a3c34eae93cc4ef53103f6a7968e9166b
Vsemv <vsemeniuk999@gmail.com>
2023-12-02 22:27:08 +0200

    start data structures: objects and arrays

---
+++ dataStructures_objectsAndArrays.js
@@ -0,0 +1,39 @@
+                // arrays
+
+let listOfNumbers = [2, 3, 5, 7, 11];
+console.log(listOfNumbers[2]);
+console.log(listOfNumbers[0]);
+console.log(listOfNumbers[2-1]);
+
+                // properties
+
+                // methods
+
+let doh = 'How how how';
+console.log(typeof doh.toUpperCase);
+console.log(doh.toUpperCase());
+
+let sequence = [1, 2, 3];
+sequence.push(4);
+sequence.push(5);
+console.log(sequence);
+console.log(sequence.pop());
+console.log(sequence);
+
+                // objects
+                console.log('dividing line----------------------------')
+
+let day1 = {
+    squirrel: false,
+    events: ['worked', 'touched the tree', 'ate pizza', 'ran']
+};
+console.log(day1.squirrel);
+console.log(day1.wolf);
+day1.wolf = false;
+console.log(day1.wolf);
+
+let descriptions = {
+    work: 'Went to work',
+    'touching the tree': 'touched the tree'
+};
+
